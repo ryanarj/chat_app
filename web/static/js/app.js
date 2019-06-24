ce1ca4434980729ac83059b7bb2cd77bbe43c394
ryanarj <karshan813@gmail.com>
2019-06-24 10:22:26 -0400

    added user

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,8 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import {Socket, Presence} from "phoenix"
+let user = document.getElementById("user").innerText
 
 // Import local files
 //
